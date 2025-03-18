ea916d3bc8430281b89855a1aaf9265b2ad170a5
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-18 15:28:12 -0300

    Add project files.

---
+++ Triangulos.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
