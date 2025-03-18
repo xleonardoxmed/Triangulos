09c19e0dc5d1e4c3661d0d878487386179491fc6
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-18 17:30:46 -0300

    Estruturada.

--- Triangulos.ConsoleApp/Program.cs
+++ Triangulos.ConsoleApp/Program.cs
@@ -1,65 +1,58 @@
-﻿namespace Triangulos.ConsoleApp
+﻿using System;
+using System.Xml.Schema;
+
+namespace Triangulos.ConsoleApp
 {
+    class TrianguleClassificator
+    {
+       public static bool validTriangule(int xside, int yside, int zside)
+        {
+            bool validSides =
+                   xside + yside > zside &&
+                   yside + zside > xside &&
+                   xside + zside > yside;
+
+            return validSides;
+        }
+
+       public static string classificate(int xside, int yside, int zside)
+        {
+            string type = "Não classificado";
+
+            if (xside == yside && xside == zside && yside == zside)
+                type = "Equilátero";
+            else if (xside != yside && xside != zside && yside != zside)
+                type = "Escaleno";
+            else
+                type = "Isóceles";
+
+            return type;
+        }
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
           while(true)
             {
-                Console.Clear();
-                Console.WriteLine("-------------------------------");
-                Console.WriteLine("Classificador de Triângulos");
-                Console.WriteLine("-------------------------------");
-                int xside, yside, zside;
-                while(true)
+                showMenu();
+                int xside = readSides(" X");
+                int yside = readSides(" Y");
+                int zside = readSides(" Z");
+                showValuesInformed(xside,yside,zside);
+
+                bool itsValid =
+                TrianguleClassificator.validTriangule(xside, yside, zside);
+
+                if (itsValid)
                 {
-                    Console.Write("Informe o valor do lado X: ");
-                    bool converted = int.TryParse(Console.ReadLine(), out xside);          
-                    if (converted)
-                        break;
-                }
-                while (true)
-                {
-                    Console.Write("Informe o valor do lado Y: ");
-                    bool converted = int.TryParse(Console.ReadLine(), out yside);                 
-                    if (converted)
-                        break;
-                }
-                while (true)
-                {
-                    Console.Write("Informe o valor do lado Z: ");
-                    bool converted = int.TryParse(Console.ReadLine(), out zside);
-                    if (converted)
-                        break;
-                }
-
-                Console.WriteLine("Valores Informados\n");
-                Console.WriteLine("Lado X: " + xside);
-                Console.WriteLine("Lado Y: " + yside);
-                Console.WriteLine("Lado Z: " + zside);
-
-                bool validSides =
-                    xside + yside > zside &&
-                    yside + zside > xside &&
-                    xside + zside > yside;
-
-                string type = "";
-                
-
-
-                if (validSides == true)
-                {
-                    if (xside == yside && xside == zside && yside == zside)
-                        type = "Equilátero";
-                    else if (xside != yside && xside != zside && yside != zside)
-                        type = "Escaleno";
-                    else
-                        type = "Isóceles";
-
-                        Console.WriteLine($"Esse triângulo é válido e é: {type}");
+                    string type = TrianguleClassificator.classificate(xside, yside, zside);
+                    Console.WriteLine($"O triângulo é {type}");
                 }
                 else
-                    Console.WriteLine(" \nERRO! \nEsses valores não respeitam a condição de existência! \n(A SOMA DE QUAISQUER DOIS LADOS NÃO PODE SER MENOR QUE O TERCEIRO VALOR)");
+                    Console.WriteLine("Erro! Os valores informados não formam um triângulo válido!");
 
                 Console.WriteLine("-------------------------------");
                 Console.WriteLine("Deseja recomeçar?:  (S/N)");
@@ -69,19 +62,40 @@
                     break;
             }
         }
-        static bool TryToConvert(string text, out int result)
+
+        static void showMenu()
         {
-            result = 0;
-            if (text != "x")
-            {
-                result = 5;
-                return true;
-            }
-            
-            return false;
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Classificador de Triângulos");
+            Console.WriteLine("-------------------------------");
         }
 
+        static int readSides(string sideName)
+        {
+            int value;
 
+            while (true)
+            {
+                Console.Write($"Informe o valor do lado{sideName}: ");
+                bool converted = int.TryParse(Console.ReadLine(), out value);
+                if (converted)
+                    break;
+            }
+            return value;
+        }
+        static void showValuesInformed(int xside, int yside,int zside)
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Valores Informados");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Lado X: " + xside);
+            Console.WriteLine("Lado Y: " + yside);
+            Console.WriteLine("Lado Z: " + zside);
+            Console.WriteLine("-------------------------------");
 
+        }
+       
+       
     }
 }
